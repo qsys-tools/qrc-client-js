9afe6a94ad63a1c9ff25e02010b221c6a23be078
James Talmage <james@talmage.io>
2026-05-18 08:16:38 -0400

    optional dependence on any-observable

--- @types/any-observable.d.ts
+++ @types/any-observable.d.ts
@@ -2,3 +2,8 @@ declare module 'any-observable' {
 	const exported: any;
 	export default exported;
 }
+
+declare module 'any-observable/optional' {
+	const exported: any;
+	export default exported;
+}
--- src/lib/poll-group.ts
+++ src/lib/poll-group.ts
@@ -1,30 +1,64 @@
-import AnyObservable from 'any-observable';
+import AnyObservable from 'any-observable/optional';
 import type QrcClient from '../qrc-client.ts';
 import type {CommandMethod, InferCommandParams, InferResponseResult} from '../validation/index.ts';
 import type {AutoPollUpdate} from '../types.ts';
-import type {ObservableConstructor, SubscriptionObserver} from './observable.ts';
+import type {
+	ObservableConstructor, SubscriptionObserver, ObservableInstance, Observer, Subscription,
+} from './observable.ts';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
 const Observable = AnyObservable as ObservableConstructor;
 
 type ChangeGroupCommand = Extract<CommandMethod, `ChangeGroup.${string}`>;
 
-export class QrcPollGroup extends Observable<AutoPollUpdate> {
+const assertObservable = () => {
+	if (!Observable) {
+		throw new Error('No observable implementation found. Please add one via `npm install zen-observable`, or install any implementation. See the `any-observable` package on npm for more details');
+	}
+};
+
+export class QrcPollGroup implements ObservableInstance<AutoPollUpdate> {
 	private readonly client: QrcClient;
 	private readonly id: string;
+	private readonly observable?: ObservableInstance<AutoPollUpdate>;
 	private observer?: SubscriptionObserver<AutoPollUpdate>;
 
 	public constructor(client: QrcClient, id: string) {
-		super(observer => {
-			this.observer = observer;
-			void this.autoPoll();
-			return () => {
-				this.observer = undefined;
-				void this.clear();
-			};
-		});
+		this.observable
+			= typeof Observable === 'function'
+				? new Observable(observer => {
+					this.observer = observer;
+					void this.autoPoll();
+					return () => {
+						this.observer = undefined;
+						void this.clear();
+					};
+				})
+				: undefined;
 		this.client = client;
 		this.id = id;
+	}
+	subscribe(
+		onNext: (value: AutoPollUpdate) => void,
+		onError?: (errorValue: Error) => void,
+		onComplete?: () => void
+	): Subscription;
+	subscribe(observer: Observer<AutoPollUpdate>): Subscription;
+
+	subscribe(...args: [Observer<AutoPollUpdate>] | [
+		onNext: (value: AutoPollUpdate) => void,
+		onError?: (errorValue: Error) => void,
+		onComplete?: () => void,
+	]) {
+		assertObservable();
+		// @ts-expect-error types are hard
+		return this.observable?.subscribe(...args);
+	}
+
+	// Returns itself
+	[Symbol.observable]() {
+		assertObservable();
+		return this.observable!;
 	}
 
 	async send<M extends ChangeGroupCommand>(method: M, parameters: Omit<InferCommandParams<M>, 'Id'>): Promise<InferResponseResult<M>> {
